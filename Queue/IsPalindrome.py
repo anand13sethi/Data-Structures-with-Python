4c8b877c81c81108ecae47b41c988ca7d2c2cdc8
Sagar <anandsethi4@gmail.com>
2017-01-18 01:07:42 +0530

    String as param rather a queue

--- Queue/IsPalindrome.py
+++ Queue/IsPalindrome.py
@@ -4,7 +4,10 @@
 from DoublyEndedQueue import Deque
 
 
-def is_palindrome(queue=Deque()):
+def is_palindrome(str):
+    queue = Deque()
+    for i in range(0, len(str)):
+        queue.enqueue_rear(str[i])
     palin = True
     while not queue.is_empty() and queue.size != 1:
         if queue.dequeue_front() != queue.dequeue_rear():
